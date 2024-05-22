9b47f3550378b5bb7900e40f25855e33d3c7917a
nKomsu <101641181+nKomsu@users.noreply.github.com>
2024-05-22 14:37:32 +0900

    Update quiz_screen.dart

--- lib/tasks/presentation/pages/quiz_screen.dart
+++ lib/tasks/presentation/pages/quiz_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: non_constant_identifier_names
+
 import 'package:flutter/material.dart';
 import '../../data/local/model/task_model.dart';
 
@@ -15,18 +17,71 @@ class _QuizScreenState extends State<QuizScreen> {
   Widget build(BuildContext context) {
     final taskModel = widget.processedTasks;
 
+    List<Map<String, String>> QnAdic = [
+      {'question': 'question1', 'answer': 'answer1'},
+      {'question': 'question2', 'answer': 'answer2'},
+      {'question': 'question3', 'answer': 'answer3'}
+    ];
+
+    List<String> quizTexts = taskModel.quizTexts!.toList();
+
+    for (var i = 0; i < QnAdic.length; i++) {
+      String slicedString = '';
+      var beforeQuestionFin = true;
+      for (var j = 0; j < quizTexts[i].length; j++) {
+        if (quizTexts[i][j] != '\n') {
+          slicedString += quizTexts[i][j];
+        } else {
+          if (beforeQuestionFin) {
+            QnAdic[i]['question'] = slicedString;
+            beforeQuestionFin = false;
+          } else {
+            QnAdic[i]['answer'] = slicedString;
+          }
+          slicedString = '';
+        }
+      }
+    }
+
     return Scaffold(
       body: Center(
         child: (taskModel.quizTexts == null || taskModel.quizTexts!.isEmpty)
             ? const Text('생성중')
             : Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text("Quiz page: ${taskModel.title}"),
-            Text("Task Description: ${taskModel.description}"),
-            ...taskModel.quizTexts!.map((text) => Text(text)).toList(),
-          ],
-        ),
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text("Quiz page: ${taskModel.title}"),
+                  Text("Task Description: ${taskModel.description}"),
+                  ...taskModel.quizTexts!.map((text) => Text(text)).toList(),
+                  ExpansionTile(
+                    shape: const Border(),
+                    title: Text(QnAdic[0]['question']!),
+                    children: <Widget>[
+                      ListTile(
+                        title: Text(QnAdic[0]['answer']!),
+                      ),
+                    ],
+                  ),
+                  ExpansionTile(
+                    shape: const Border(),
+                    title: Text(QnAdic[1]['question']!),
+                    children: <Widget>[
+                      ListTile(
+                        title: Text(QnAdic[1]['answer']!),
+                      ),
+                    ],
+                  ),
+                  ExpansionTile(
+                    shape: const Border(),
+                    title: Text(QnAdic[2]['question']!),
+                    children: <Widget>[
+                      ListTile(
+                        title: Text(QnAdic[2]['answer']!),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
       ),
     );
   }
