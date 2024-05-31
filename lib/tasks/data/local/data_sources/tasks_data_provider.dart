f5d72b1bbd715655c178c8a631a54717dafe67a0
nKomsu <101641181+nKomsu@users.noreply.github.com>
2024-05-31 20:09:52 +0900

    multiple choice task

--- lib/tasks/data/local/data_sources/tasks_data_provider.dart
+++ lib/tasks/data/local/data_sources/tasks_data_provider.dart
@@ -58,7 +58,7 @@ class TaskDataProvider {
         });
         break;
       case 1:
-      //sort by completed tasks
+        //sort by completed tasks
         tasks.sort((a, b) {
           if (!a.completed && b.completed) {
             return 1;
@@ -69,7 +69,7 @@ class TaskDataProvider {
         });
         break;
       case 2:
-      //sort by pending tasks
+        //sort by pending tasks
         tasks.sort((a, b) {
           if (a.completed == b.completed) {
             return 0;
@@ -90,7 +90,7 @@ class TaskDataProvider {
       tasks.add(taskModel);
       //요건 main.dart에서 언급한 로컬 저장소에 변경된 tasks를 넣음
       final List<String> taskJsonList =
-      tasks.map((task) => json.encode(task.toJson())).toList();
+          tasks.map((task) => json.encode(task.toJson())).toList();
       await prefs!.setStringList(Constants.taskKey, taskJsonList);
     } catch (exception) {
       throw Exception(handleException(exception));
@@ -114,7 +114,7 @@ class TaskDataProvider {
       });
       // 애도 로컬에 다시 저장
       final List<String> taskJsonList =
-      tasks.map((task) => json.encode(task.toJson())).toList();
+          tasks.map((task) => json.encode(task.toJson())).toList();
       prefs!.setStringList(Constants.taskKey, taskJsonList);
       return tasks;
     } catch (exception) {
@@ -127,7 +127,7 @@ class TaskDataProvider {
     try {
       tasks.remove(taskModel);
       final List<String> taskJsonList =
-      tasks.map((task) => json.encode(task.toJson())).toList();
+          tasks.map((task) => json.encode(task.toJson())).toList();
       prefs!.setStringList(Constants.taskKey, taskJsonList);
       return tasks;
     } catch (exception) {
@@ -143,24 +143,25 @@ class TaskDataProvider {
     return matchedTasked.where((task) {
       final titleMatches = task.title.toLowerCase().contains(searchText);
       final descriptionMatches =
-      task.description.toLowerCase().contains(searchText);
+          task.description.toLowerCase().contains(searchText);
       return titleMatches || descriptionMatches;
     }).toList();
   }
 
   Future<TaskModel> processTasks(TaskModel taskModel) async {
     // 문백별로 나누는 코드
-    taskModel.splitTranscribedTextsByContext=await _splitTranscribedTextByContext(taskModel.transcribedTexts);
+    taskModel.splitTranscribedTextsByContext =
+        await _splitTranscribedTextByContext(taskModel.transcribedTexts);
 
     //text to summary 코드
-    taskModel.summaryTexts =
-    await Future.wait(taskModel.splitTranscribedTextsByContext!.map((myString) async {
+    taskModel.summaryTexts = await Future.wait(
+        taskModel.splitTranscribedTextsByContext!.map((myString) async {
       return "gpt@${await _summaryTasks(input: myString)}";
     }).toList());
 
     // summary to quiz 코드
-    taskModel.quizTexts =
-    await Future.wait(taskModel.splitTranscribedTextsByContext!.map((myString) async {
+    taskModel.quizTexts = await Future.wait(
+        taskModel.splitTranscribedTextsByContext!.map((myString) async {
       return await _quizTasks(input: myString);
     }).toList());
 
@@ -185,12 +186,51 @@ class TaskDataProvider {
           {
             "role": "system",
             "content":
-            "너는 user가 보낸 글의 내용을 기반으로 주관식 문제를 만들어내는 봇이야. 문제를 만들고 해답을 알려 줘."
+                "너는 user가 보낸 글의 내용을 기반으로 주관식 문제를 만들어내는 봇이야. 문제를 만들고 해답을 알려 줘."
           },
           {"role": "assistant", "content": "문제 : \n해답 : \n"},
           {"role": "user", "content": input}
         ],
         'max_tokens': 300,
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      final decoded = jsonDecode(utf8.decode(response.bodyBytes));
+      final quiz = decoded['choices'][0]['message']['content'] as String;
+      return quiz;
+    } else {
+      throw Exception('Failed to summarize text');
+    }
+  }
+
+  //객관식
+  Future<String> _multipleChoiceTasks({required String input}) async {
+    final apiKey = dotenv.env['API_KEY'];
+    const endpoint = 'https://api.openai.com/v1/chat/completions';
+
+    final response = await http.post(
+      Uri.parse(endpoint),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $apiKey',
+      },
+      body: jsonEncode({
+        'model': 'gpt-3.5-turbo',
+        "messages": [
+          {
+            "role": "system",
+            "content":
+                "너는 user가 보낸 글의 내용을 4-multiple choice 문제를 만들어내는 봇이야. assistant에 없는 개행문자를 추가하지 말아 줘."
+          },
+          {
+            "role": "assistant",
+            "content":
+                "문제 : 다음 중 가장 큰 행성은?\n1. 목성\n2. 지구\n3. 화성\n4. 천왕성\n해답 : 1. 목성"
+          },
+          {"role": "user", "content": input}
+        ],
+        'max_tokens': 1000,
       }),
     );
 
@@ -234,25 +274,25 @@ class TaskDataProvider {
 
   // TODO 통짜 스트링 문맥별로 나누는 것
   Future<List<String>> _splitTranscribedTextByContext(String text) async {
-    List<String> splitHalfList=_splitTextHalf(text);
+    List<String> splitHalfList = _splitTextHalf(text);
 
     List<String> splitByContextList =
-    await Future.wait(splitHalfList.map((myString) async {
+        await Future.wait(splitHalfList.map((myString) async {
       return await _splitTextByContextUsingGPT(input: myString);
     }).toList());
-    String splitByContext=splitByContextList.join("");
-    splitByContext=splitByContext.replaceAll("\n","");
+    String splitByContext = splitByContextList.join("");
+    splitByContext = splitByContext.replaceAll("\n", "");
 
-    List<String> result=splitByContext.split('#');
+    List<String> result = splitByContext.split('#');
     return result;
   }
 
 // TODO 스트링 반갈
-  List<String> _splitTextHalf(String text){
-    List<String> result=[];
-    var tempIndex=text.lastIndexOf('. ',text.length~/2);
-    result.add(text.substring(0,tempIndex+2));
-    result.add(text.substring(tempIndex+2));
+  List<String> _splitTextHalf(String text) {
+    List<String> result = [];
+    var tempIndex = text.lastIndexOf('. ', text.length ~/ 2);
+    result.add(text.substring(0, tempIndex + 2));
+    result.add(text.substring(tempIndex + 2));
     return result;
   }
 
@@ -270,7 +310,10 @@ class TaskDataProvider {
       body: jsonEncode({
         'model': 'gpt-3.5-turbo',
         "messages": [
-          {"role": "system", "content": "이 글을 문맥별로 나누어주세요. 문맥 사이에는 # 기호를 넣어주세요."},
+          {
+            "role": "system",
+            "content": "이 글을 문맥별로 나누어주세요. 문맥 사이에는 # 기호를 넣어주세요."
+          },
           {"role": "user", "content": input}
         ]
         // 'max_tokens': 50, // Adjust the summary length as needed
